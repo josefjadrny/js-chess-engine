52566b93c46ac8822934be18d88fc3f546774dc1
Josef Jadrny <bc.josefjadrny@gmail.com>
2020-04-25 15:14:24 +0200

    feat: Added status function for retrieve game status info (checkmate, isFinished)

--- example/server.js
+++ example/server.js
@@ -1,6 +1,6 @@
 import fastify from 'fastify'
 import * as cors from 'fastify-cors'
-import { getMoves } from '../lib/js-chess.js'
+import { getMoves, getStatus } from '../lib/js-chess.js'
 
 const server = fastify({
     logger: true
@@ -9,6 +9,15 @@ const server = fastify({
 server.post('/moves', (request, response) => {
     try {
         const result = getMoves(request.body)
+        response.send(result)
+    } catch (error) {
+        response.code(404).send(error)
+    }
+})
+
+server.post('/status', (request, response) => {
+    try {
+        const result = getStatus(request.body)
         response.send(result)
     } catch (error) {
         response.code(404).send(error)
