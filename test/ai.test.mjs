5e24a05fef9c8e991f2f6387d47949bb65b62297
Josef Jadrny <bc.josefjadrny@gmail.com>
2020-05-11 11:10:09 +0200

    feat: Checkmate logic improved - part2

--- test/ai.test.mjs
+++ test/ai.test.mjs
@@ -10,7 +10,7 @@ const expect = chai.expect
 
 describe('Should properly calculate BEST AI move for 4 future moves', function () {
     before(function () {
-        this.aiLevel = 2
+        this.aiLevel = 3
     })
 
     it.skip('Black should move with pawn E7 to E5', function () {
@@ -20,7 +20,7 @@ describe('Should properly calculate BEST AI move for 4 future moves', function (
         expect(game.board.calculateAiMove(this.aiLevel)).to.deep.include({ from: 'E7', to: 'E5' })
     })
 
-    it('Should do checkmate', function () {
+    it('Should do checkmate in one move', function () {
         const game = new Game({
             pieces: {
                 E1: 'K',
@@ -30,6 +30,22 @@ describe('Should properly calculate BEST AI move for 4 future moves', function (
             },
         })
         game.printToConsole()
-        expect(game.board.calculateAiMove(this.aiLevel)).to.deep.include({ from: 'B6', to: 'B8' })
+        const nextMove = game.board.calculateAiMove(this.aiLevel)
+        expect(nextMove).to.deep.include({ from: 'B6' })
+    })
+
+    it('Should do checkmate in tree moves', function () {
+        const game = new Game({
+            pieces: {
+                E1: 'K',
+                A7: 'R',
+                A6: 'R',
+                E8: 'k',
+            },
+        })
+        game.printToConsole()
+        const nextMove = game.board.calculateAiMove(this.aiLevel)
+        expect(game.board.calculateAiMove(this.aiLevel)).to.deep.include({ from: 'A6' })
+        expect(nextMove.to).to.be.oneOf(['B6', 'C6', 'D6', 'F6', 'G6', 'H6'])
     })
 })
